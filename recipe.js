e74723a5645caa6f299f6b5b85614607e8404cba
irevived1 <haifeng.wu@jjay.cuny.edu>
2016-09-03 23:35:04 -0400

    Done.

--- recipe.js
+++ recipe.js
@@ -1,1 +1,23 @@
 'use strict';
+
+function addIngredient(recipe,ingre,quantity) {
+  recipe[ingre] = quantity;
+  return recipe;
+}
+
+function removeIngredient(recipe,ingre) {
+  delete recipe[ingre];
+  return recipe;
+}
+
+function updateIngredient(recipe,ingre,quantity) {
+  recipe[ingre] = quantity;
+  return recipe;
+}
+
+function readRecipe(recipe) {
+  for (var key in recipe) {
+    console.log("this recipe calls for " + recipe[key] + " of " + key);
+  }
+  return recipe;
+}
